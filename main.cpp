66db8ed831f6739f85648194c11d046731f68a41
Bicho15H <bichohelou15@gmail.com>
2022-11-04 15:48:22 +0200

    removed getters

--- main.cpp
+++ main.cpp
@@ -7,12 +7,6 @@ public:
     Person(string name, int age) : name(name), age(age) {
     }
 
-    string getName() const {
-        return name;
-    }
-    int getAge() const {
-        return age;
-    }
 
     void afficher() const{
         cout << "Nom:" << name << endl;
